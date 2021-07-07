990f98cf1713c9f429eb840ad82e86faa129f075
Artem <artemq4@yandex.ru>
2021-07-07 12:59:25 +0500

    service адаптив готов

--- Source/JS/blocks/swiper.js
+++ Source/JS/blocks/swiper.js
@@ -1,10 +1,33 @@
 new Swiper('.service__swiper', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 30,
+    pagination: {
+        el: '.service__pagination',
+        clickable: true,
+    },
 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
+    breakpoints: {
+        610: {
+            slidesPerView: 2,
+        },
+
+        992: {
+            slidesPerView: 3,
+        },
+
+        1100: {
+            slidesPerView: 3,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
+        },
+
+        1400: {
+            slidesPerView: 4,
+        },
+    },
+
+
 });
