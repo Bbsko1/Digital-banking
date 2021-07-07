b296ed24d08d1a8636a8dc8390507f9b8953020d
Artem <artemq4@yandex.ru>
2021-07-07 11:28:57 +0500

    get-card адаптив готов

--- Source/JS/blocks/navButton.js
+++ Source/JS/blocks/navButton.js
@@ -1,16 +1,16 @@
 const navBtn = document.querySelector(".navigation__button");
-const navList = document.querySelector(".navigation__list");
+const navList = document.querySelector(".navigation__wrapper");
 const menuLinks = document.querySelectorAll('.navigation__link');
 
 navBtn.addEventListener("click", function (evt) {
     evt.preventDefault()
     navBtn.classList.toggle("navigation__button--closed");
-    navList.classList.toggle("navigation__list--active")
+    navList.classList.toggle("navigation__wrapper--active")
 })
 
 menuLinks.forEach(function (link) {
     link.addEventListener('click', function() {
-        navList.classList.remove("navigation__list--active")
+        navList.classList.remove("navigation__wrapper--active")
         navBtn.classList.add("navigation__button--closed");
     })
 })
